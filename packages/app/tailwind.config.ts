1597444757ec62edf5c9bcb2de8f9e92352ef2f9
Amon Sawamura <amotarao@gmail.com>
2023-05-08 16:49:32 +0900

    srcDir

--- packages/app/tailwind.config.ts
+++ packages/app/tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
