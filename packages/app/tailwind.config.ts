2544d4d0d0198e30dd7b7d24e97671eb3a1b50e3
Amon Sawamura <amotarao@gmail.com>
2023-05-08 16:49:22 +0900

    tailwind.config.ts

--- packages/app/tailwind.config.ts
+++ packages/app/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+export default {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -9,4 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
