15a9e1f2dea6a61c900f22cef4761ee5a0e18c2f
Amon Sawamura <amotarao@gmail.com>
2023-05-08 16:51:33 +0900

    App Router

---
+++ packages/app/src/app/layout.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.scss';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja-JP">
+      <body>{children}</body>
+    </html>
+  );
+}
